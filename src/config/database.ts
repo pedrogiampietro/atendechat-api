070a9fbbe70f97e8d2aa367693cb33d905220442
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-06-21 21:07:18 -0300

    fix: ajusts in ssl mode to running in deploy

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,12 +5,18 @@ module.exports = {
     charset: "utf8mb4",
     collate: "utf8mb4_bin"
   },
-  dialect: process.env.DB_DIALECT || "mysql",
+  dialect: process.env.DB_DIALECT || "postgres", // Ajustando para PostgreSQL
   timezone: "-03:00",
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 5432, // Ajustando para a porta padrão do PostgreSQL
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  logging: process.env.DB_DEBUG === "true"
+  logging: process.env.DB_DEBUG === "true",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Pode ser ajustado conforme a necessidade do seu certificado SSL
+    }
+  }
 };
